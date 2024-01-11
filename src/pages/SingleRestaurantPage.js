c8ce26a87d2fe95bd5769a5cf864d20c989e69bf
himanshu7377 <him.singh7069@gmail.com>
2024-01-11 11:43:48 +0530

    fourth

--- src/pages/SingleRestaurantPage.js
+++ src/pages/SingleRestaurantPage.js
@@ -9,7 +9,7 @@ const SingleRestaurantPage = () => {
   useEffect(() => {
     // Retrieve token from localStorage
     const token = localStorage.getItem("token");
-  
+
     // Fetch the list of all restaurants
     axios
       .get(`https://staging.fastor.in/v1/m/restaurant`, {
@@ -19,25 +19,23 @@ const SingleRestaurantPage = () => {
       })
       .then((response) => {
         // Log the entire response data to the console for inspection
-        console.log('Response Data:', response.data);
-  
+        console.log("Response Data:", response.data);
+
         // Find the restaurant with the matching ID
         const foundRestaurant = response.data.find(
           (restaurant) => restaurant.restaurant_id === restaurantId
         );
-  
+
         // Log the found restaurant to the console for inspection
-        console.log('Found Restaurant:', foundRestaurant);
-  
+        console.log("Found Restaurant:", foundRestaurant);
+
         // Set the found restaurant in the state
         setRestaurant(foundRestaurant);
       })
       .catch((error) => console.error(error));
   }, [restaurantId]);
-  
 
-  
-  console.log(restaurant)
+  console.log(restaurant);
   if (!restaurant) {
     return <p>Loading...</p>;
   }
@@ -53,9 +51,9 @@ const SingleRestaurantPage = () => {
   } = restaurant;
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+    <div className="flex flex-col items-center mt-5">
       {/* Upper part with image */}
-      <div style={{ height: "60%", overflow: "hidden" }}>
+      <div style={{ height: "400px", overflow: "hidden", width: "375px" }}>
         {images && images.length > 0 && (
           <img
             src={images[0]?.url}
@@ -66,29 +64,46 @@ const SingleRestaurantPage = () => {
       </div>
 
       {/* Lower part with details */}
-      <div style={{ flex: 1, padding: "10px", borderTop: "1px solid #ccc" }}>
-        <h1>{restaurant_name}</h1>
-        <p>
-          <strong>Cuisine:</strong>{" "}
-          {cuisines
-            ? cuisines.map((cuisine) => cuisine.cuisine_name).join(", ")
-            : "N/A"}
-        </p>
-        <p>
-          <strong>Address:</strong>{" "}
-          {location ? ` ${location.location_locality}` : "N/A"}
-        </p>
-        <p>
-          <strong>Rating:</strong>{" "}
-          {rating ? rating.restaurant_avg_rating : "N/A"}
-        </p>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <p>
-            <strong>Average Cost for Two:</strong>{" "}
-            {avg_cost_for_two
-              ? `${avg_cost_for_two} ${currency.symbol}`
-              : "N/A"}
-          </p>
+      <div className="bg-white flex flex-col gap-12 w-[375px] h-[520px] font-['Urbanist'] items-start pl-8 py-4 rounded-tl-[25px] rounded-tr-[25px]">
+        <div className="flex flex-col ml-px gap-2 w-5/6 items-start">
+          <div className="flex flex-row gap-20 w-full items-start">
+            <div className="flex flex-col w-2/3 items-start">
+              <div className="text-lg font-bold text-[#1e232c]">
+                {restaurant_name}
+              </div>
+              <div className="font-medium text-[#505259]">
+                {location
+                  ? `${location.location_locality} ${location.city_name}`
+                  : "N/A"}
+              </div>
+            </div>
+            <div className="flex flex-row mt-3 gap-2 w-10 items-start">
+              <img
+                src="https://file.rendit.io/n/blXn1fvm6a8TFJ8pFkCX.svg"
+                alt="Star"
+                id="Star"
+                className="w-4"
+              />
+              <div className="text-xs font-medium text-[#595959] mt-px">
+                {rating ? rating.restaurant_avg_rating : "N/A"}
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row ml-px gap-2 w-2/5 items-start">
+            <img
+              src="https://file.rendit.io/n/xBmx8DpLzC07V3retf0r.svg"
+              alt="Teenyiconsdiscountoutline"
+              className="w-4"
+            />
+            <div className="text-xs font-semibold text-[#d39171] mt-px">
+             <p>4 Offers Trending</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="text-sm font-['Urbanist'] font-medium leading-[16.8px] text-[#515151] w-full">
+          Our delicate vanilla cake swirled with chocolate and filled with mocha
+          chocolate chip cream and a layer of dark chocolate ganache
         </div>
       </div>
     </div>
